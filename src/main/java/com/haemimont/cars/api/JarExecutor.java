b89cbc122cd0d609bb98f87304a1be55cd57325d
Atanas-Tonchev <flirtadjia.at@gmail.com>
2023-07-25 13:25:53 +0300

    Add comments into JarExecutor class

--- src/main/java/com/haemimont/cars/api/JarExecutor.java
+++ src/main/java/com/haemimont/cars/api/JarExecutor.java
@@ -9,27 +9,36 @@ public class JarExecutor {
     private static final Logger logger = LogManager.getLogger(JarExecutor.class);
 
     public static List<String> startExtJarProgram() {
+        //create new list for reverse the data from result list
         List<String> listReverse = new ArrayList<>();
+        //declaration commands for jar executor
         String[] commands = {"java", "-jar", "cars.jar"};
+        //create process builder with the commands
         ProcessBuilder pb = new ProcessBuilder(commands);
+        //chose a path of .jar file
         File selectedFile = new File(ApiPathConfiguration.PATH_JAR_FILE);
+        //redirect error to error.txt
         pb.redirectError(new File(Paths.get(ApiPathConfiguration.PATH_JAR_ERROR_FILE).toUri()));
+        //get .jar file
         pb.directory(selectedFile.getAbsoluteFile());
         try {
+            //create process and start process builder
             final Process process = pb.start();
-
+            //create exit status
             final int exitStatus = process.waitFor();
 
             if (exitStatus == 0) {
+                //get data from logger file
                 listReverse = LoggerFile.getLoggJarFile(ApiPathConfiguration.PATH_JAR_LOGGER_FILE);
             } else {
+                //get data from error file
                 listReverse = LoggerFile.getLoggJarFile(ApiPathConfiguration.PATH_JAR_ERROR_FILE);
             }
 
         } catch (InterruptedException | IOException ex) {
             logger.error("InterruptedException: " + ex.getMessage());
         }
-
+        //return data after check status
         return listReverse;
     }
 }
